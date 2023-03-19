abdadf6273e6d774b6d2fa60514670c49d8b3bf1
jungseungjin <ywack@naver.com>
2023-03-19 18:31:49 +0900

    공통 헤더 만들기

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import styled from "styled-components"
-
+import Header from "../components/Header"
 const Container = styled.div`
     font-size:21px;
     color:gray;
 `
 
 const index:React.FC = () => {
-    return <Container>hello world</Container>
+    return <Container>
+        <Header/>
+        
+    </Container>
 }
 export default index;
